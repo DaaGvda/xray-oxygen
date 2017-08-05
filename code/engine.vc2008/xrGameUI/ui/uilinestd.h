0d0c8bb9a4a38fe4e65d4ea1699eea3d2922548f
TomPhantom <q3106584pasha>
2017-08-06 03:25:18 +0400

    * RELEASED: 1. - Release local dll of UI system. 2. - Local files position (maybe not original):      - UITreeViewItem -> "UI\UIWindows\TreeView\"      - UILanimController -> "UI\"      - UIAnimatedStatic -> "UI\"      - IUWnpParams -> "UI\Common\Inventory\"      - ArtefactDetectorUI -> "UI\Common\MainInGame\"      - FractionState -> "UI\Common\MainInGame\" ////////////////////////////////////////////////////////////////// * Создано: 1. - Реализована библиотека, в которой содержится всё описание UI. 2. - Расположение файлов (возможно оригинальных):      - UITreeViewItem -> "UI\UIWindows\TreeView\"      - UILanimController -> "UI\"      - UIAnimatedStatic -> "UI\"      - IUWnpParams -> "UI\Common\Inventory\"      - ArtefactDetectorUI -> "UI\Common\MainInGame\"      - FractionState -> "UI\Common\MainInGame\"

---
+++ code/engine.vc2008/xrGameUI/ui/uilinestd.h
@@ -0,0 +1,8 @@
+using StrSize = xr_string::size_type;
+
+
+const char* const BEGIN = "%c[";
+const char* const END =  "]";
+//#define xr_string::npos npos
+
+const StrSize npos = (StrSize)xr_string::npos;
---
+++ code/engine.vc2008/xrGameUI/xrGameUI.hpp
@@ -0,0 +1,15 @@
+#include "../xrCore/xrCore.h"
+
+
+
+
+
+
+
+
+
+#ifdef XRGAME_UI_EXPORTS
+#define XRGAME_UI_API __declspec(dllexport)
+#else
+#define XRGAME_UI_API __declspec(dllimport)
+#endif
